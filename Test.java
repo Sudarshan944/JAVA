99a9d84841cc265117307cc97cc08a189e997f78
Sudarshan Reddy <88623298+Sudarshan944@users.noreply.github.com>
2023-05-08 10:07:51 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -23,15 +23,33 @@ public static void main(String[] args) {
 //	int a=5;
 //	System.out.println(a++);
 //	System.out.println(++a);
-	int a=10;
-	int b=25;
-	int c=35;
-	if (a>b && a>c){
-		System.out.println("A is greater than B and C");
-	}else if (b>c){
-		System.out.println("B is greater than A and C");
-	}else {
-		System.out.println("C is greater than A and B");
-	}
+//	int a=10;
+//	int b=25;
+//	int c=35;
+//	if (a>b && a>c){
+//		System.out.println("A is greater than B and C");
+//	}else if (b>c){
+//		System.out.println("B is greater than A and C");
+//	}else {
+//		System.out.println("C is greater than A and B");
+//	}
+//	for(int i=0;i<5;) {
+//		System.out.println(++i);
+//	}
+//	int i=0;
+//	while(i<5) {
+//		i++;
+//		System.out.print(i+" ");
+//	}
+	/**
+	 * Types of errors
+	 * 1.Compile Time Error -- fails to compile -- syntatical errors, and other
+	 * 2.Runtime Error -- inefficiency of the computer to execute a particular statement -- exceptions
+	 * some exceptions are found by compile time are called checked exceptions
+	 * exceptions are not found by compile time (or) found by jvm are called unchecked exceptions
+	 * unchecked are considered to be recoverable
+	 * checked exception are able to be handled
+	 * IOException -> checked exception
+	 **/
 }
 }
